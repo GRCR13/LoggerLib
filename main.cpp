ef4681fbf5c315b386f318a8f40dd172f6f19c0c
GRCR13 <78537712+GRCR13@users.noreply.github.com>
2025-07-20 21:13:42 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <thread>
+#include <iostream>
+#include <vector>
+#include "lib/logger.h"
+using std::string;
+
+int main(int argc, char* argv[]) {
+
+    string filename = (argc > 1 ? argv[1] : "log.txt");
+
+    LogLvl lvl = LogLvl::debug;
+    if (argc > 2) {
+        string s = argv[2];
+        if (s == "warning") lvl = LogLvl::warning;
+        else if (s == "error") lvl = LogLvl::error;
+    }
+
+    Logger logger(filename, lvl);
+ 
+    std::vector<std::thread> threads;
+    string message;
+    
+    for (int t = 0; t < 12; ++t) {
+        threads.emplace_back([&logger, lvl, &message ,t]() {
+
+            for (int i = 0; i < 10; ++i) {
+                //Ожидать нового ввода от пользователя после передачи данных
+                std::cin >> message;
+                logger.Write(message, lvl);
+            }
+
+            });
+    }
+
+    for (auto& th : threads) {
+        if (th.joinable())
+        {
+            th.join();
+        }
+    }
+
+    return 0;
+}
